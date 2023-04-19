bc5110e5747de171359db3e371e16e9ff65f146c
dl5 <user.dl5>
2023-04-19 15:24:29 +0400

    remove one grep command

--- homework/myscript.sh
+++ homework/myscript.sh
@@ -12,5 +12,5 @@ echo "total processes: $(ps -ef | tail -n +2 | wc -l)"
 
 echo "ssh: $(ps -ef | grep ssh | grep -v 'grep' | wc -l)"
 
-echo "group permissions: $(ls -l /etc/group | grep ^- | awk '{print $1}')"
+echo "group permissions: $(ls -l /etc/group | awk '{print $1}')"
 
